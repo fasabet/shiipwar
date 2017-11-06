cdf45e39bb46210014bf2d197b9f61dc7056cd98
fasabet@gmail.com <fasabet@gmail.com>
2017-11-06 09:23:27 +0100

    starting

---
+++ farHTML.js
@@ -0,0 +1,17 @@
+var rows = 6;
+var colon = 7;
+var squareSize = 50;
+var Board = document.getElementById("board");
+
+for ($row=1;$row<=6;$row++){
+   for($colon=1;$colon<=7;$colon++){
+     
+    var square = document.createElement("div");
+        gameBoardContainer.appendChild(square);
+        
+    }     
+    echo
+    "</table>";
+   
+    echo"<br>";
+  }
